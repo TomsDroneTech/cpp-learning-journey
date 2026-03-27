b7fd9dc6ceb9ab180f08eb3730310ccae505cd86
Burabari Tom <burabaritom09@gmail.com>
2026-03-27 16:58:29 +0100

    Personalizes initial greeting output
    Updates the `hello_world` program to include a newline character and personal identification details in the console output. This enhances the initial greeting by making it more informative and customized.

--- chapter-01/hello_world.cpp
+++ chapter-01/hello_world.cpp
@@ -3,7 +3,7 @@
 int main()
 {
     
-    std::cout<<"Hello world";
-    
+    std::cout<<"Hello world' \n";
+    std::cout<<" My name is Tom, I'm a drone engineering student at Fh kufstein Tirol Austria";
     return 0;
 }
